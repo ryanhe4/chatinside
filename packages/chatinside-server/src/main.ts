4d01a756912939d86adffa12bd8cdc6e7a2c7a51
heechan <a3_clobot@clobot.co.kr>
2022-11-29 19:31:16 +0900

    feat: integrate prisma and partially implements registration

--- packages/chatinside-server/src/main.ts
+++ packages/chatinside-server/src/main.ts
@@ -2,6 +2,7 @@ import Fastify from 'fastify'
 import fastifySwagger from '@fastify/swagger'
 import routes from './routes/index.js'
 import { swaggerConfig } from './config/swagger.js'
+import db from './lib/db.js'
 
 const server = Fastify({
   logger: true,
