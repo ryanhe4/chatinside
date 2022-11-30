02eb5b6dd64dc0b37419125965e24f180d6a7ceb
heechan <ryanhe4@gmail.com>
2022-11-30 21:23:58 +0900

    feat: implements requireAuthPlugin

--- packages/chatinside-server/src/routes/api/me/index.ts
+++ packages/chatinside-server/src/routes/api/me/index.ts
@@ -1,18 +1,11 @@
 import { FastifyPluginAsync } from 'fastify'
 import { getMeSchema } from './schema.js'
 import AppError from '../../../lib/AppError.js'
+import requireAuthPlugin from '../../../plugins/requireAuthPlugin.js'
+
 export const meRoute: FastifyPluginAsync = async (fastify) => {
+  fastify.register(requireAuthPlugin)
   fastify.get('/', { schema: getMeSchema }, async (request) => {
-    if (request.isExpiredToken) {
-      throw new AppError('UnauthorizedError', {
-        isExpiredToken: true,
-      })
-    }
-    if (!request.user) {
-      throw new AppError('UnauthorizedError', {
-        isExpiredToken: false,
-      })
-    }
     return request.user
   })
 }
